a4e374a6076b98d76282a906f21bb684fabe4ffc
Rory <coderory@gmail.com>
2020-04-17 14:25:55 +0200

    descanso

--- src/components/Opos/Opos.js
+++ src/components/Opos/Opos.js
@@ -3,24 +3,26 @@ import OpoI1 from '../Opos/OpoI1.png';
 import OpoI2 from '../Opos/OpoI2.png';
 import OpoI3 from '../Opos/OpoI3.png';
 import OpoI4 from '../Opos/OpoI4.png';
+import '../Opos/Opos.css';
 
 
 
 
 
-class Opo1 extends React.Component {
+class Opos extends React.Component {
     
     render() {
         return(
-            <div>
-                <img src={OpoI1} alt="Primera opo a elegir" height="280vmin" width="325vmin"/>      
-                <img src={OpoI2} alt="Primera opo a elegir" height="280vmin" width="325vmin"/> 
-                <img src={OpoI3} alt="Primera opo a elegir" height="280vmin" width="325vmin"/>
-                <img src={OpoI4} alt="Primera opo a elegir" height="280vmin" width="325vmin"/>    
-         
+            <div className="opos">
+                <ul>
+                    <ul> <a href="">  <img src={OpoI1} alt="Primera opo a elegir" height="280vmin" width="325vmin"/></a></ul>
+                    <ul><a href=""><img src={OpoI2} alt="Primera opo a elegir" height="280vmin" width="325vmin"/> </a></ul>
+                    <ul><a href=""><img src={OpoI3} alt="Primera opo a elegir" height="280vmin" width="325vmin"/></a></ul>
+                    <ul><a href=""><img src={OpoI4} alt="Primera opo a elegir" height="280vmin" width="325vmin"/> </a></ul> 
+                </ul>
             </div>
         );
     }
 }
 
-export default Opo1;
+export default Opos;
