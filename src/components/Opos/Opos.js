29dca7bb2d2e46d5dc383d78834df13e3cafee08
Rory <coderory@gmail.com>
2020-04-20 17:08:38 +0200

    Avanzando

--- src/components/Opos/Opos.js
+++ src/components/Opos/Opos.js
@@ -15,11 +15,16 @@ class Opos extends React.Component {
         return(
             <div className="maincontainer">
                 <div className="thecard">
-                    <div className="thefront"><img src={OpoI1} alt="Primera opo a elegir" height="280vmin" width="325vmin"/></div>
-                    <div className="theback"><a href=""><img src={OpoI1} alt="Primera opo a elegir" height="280vmin" width="325vmin"/></a></div>             
-                
-                </div>
-               
+                    <div className="thefront"><img src={OpoI1} alt="Primera opo a elegir" height="320px" width="320px"/></div>
+                    <div className="theback">
+                        <ul className="thebackinformation">
+                            <h1 id="opo1h1"><strong>Oposición 1</strong></h1>
+                            <h2><a href="">Información</a></h2>
+                            <h3><a href="">Temario y Grupos</a></h3>
+                                
+                        </ul>                         
+                    </div>                
+                </div>               
             </div>
         );
     }
