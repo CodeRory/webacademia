f167353e6b2e02ef49509bc2dbdb111bfd16d6b9
Rory <coderory@gmail.com>
2020-04-22 14:02:57 +0200

    Comienzo del slide images

---
+++ src/components/slider/Slider.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Slider extends React.Component {
+
+    render(){
+        return(
+            <div className="slider">
+                <ul>
+                    <li><img src="" alt=""/></li>
+                    <li><img src="" alt=""/></li>
+                    <li><img src="" alt=""/></li>
+                    <li><img src="" alt=""/></li>             
+                </ul>            
+            
+            </div>
+
+
+
+
+
+        )
+    }
+}
