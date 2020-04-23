8715da9a883f0f44e2cacc2458cb29c726146a47
Rory <coderory@gmail.com>
2020-04-23 13:13:13 +0200

    Avances en el slider

--- src/components/slider/content.js
+++ src/components/slider/content.js
@@ -1,56 +1,43 @@
+import study1 from '../slider/study1.png';
+import study2 from '../slider/study2.png';
+import study3 from '../slider/study3.png';
+import study4 from '../slider/study4.png';
+
+
 export default [
 	{
-		title: 'Vulputate Mollis Ultricies',
+		title: 'Lorem Ipsum',
 		description:
-			'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+			'Aliquam vitae mi nec est congue ultricies. Donec at risus vitae nulla mollis tincidunt.',
 		button: 'Read More',
-		image: 'https://i.imgur.com/ZXBtVw7.jpg',
-		user: 'Daniel',
-		userProfile: 'https://s7.postimg.cc/abavelo3v/1_3x.png',
+		image: study1,
+		
+		
 	},
 	{
-		title: 'Tortor Dapibus',
+		title: 'Suspendisse vel diam metus. ',
 		description:
-			'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur purus sit amet fermentum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.',
+			'rci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus',
 		button: 'Discover',
-		image: 'https://i.imgur.com/DCdBXcq.jpg',
-		user: 'Samantha',
-		userProfile: 'https://s7.postimg.cc/ujy8zz7vv/5_3x.png',
+		image: study2,
+		
+		
 	},
 	{
-		title: 'Phasellus volutpat metus',
+		title: 'Phasellus vitae est lacus.',
 		description:
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam.',
-		button: 'Buy now',
-		image: 'https://i.imgur.com/DvmN8Hx.jpg',
-		user: 'Michael',
-		userProfile: 'https://s7.postimg.cc/6exjimijv/3_3x.png',
+			'Aliquam semper consequat ligula, ut ornare felis eleifend a. Donec scelerisque sed dui sit amet sollicitudin.',
+		button: 'Try now',
+		image: study3,
+		
 	},
 	{
-		title: 'Ultricies Vulputate Mollis',
+		title: 'Curabitur placerat',
 		description:
-			'Aenean eu leo quam. Pellentesque ornare sem lacinia  nibh, ut fermentum massa justo sit amet risus. Cras justo odio, dapibus ac facilisis.',
+			'Morbi vehicula dignissim velit vitae dignissim.',
 		button: 'Read More',
-		image: 'https://i.imgur.com/ZXBtVw7.jpg',
-		user: 'Jessica',
-		userProfile: 'https://s7.postimg.cc/7ihnu80ij/4_3x.png',
-	},
-	{
-		title: 'Odo Aenean Quam Tortor',
-		description:
-			'Nullam id dolor id nibh ultricies vehicula ut id elit. Cras mattis consectetur Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-		button: 'Discover',
-		image: 'https://i.imgur.com/DCdBXcq.jpg',
-		user: 'William',
-		userProfile: 'https://s7.postimg.cc/f9ydt4zmj/2_3x.png',
-	},
-	{
-		title: 'Volutpat Aenean metus',
-		description:
-			'quam venenatis vestibulum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentumconsectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis.',
-		button: 'Buy now',
-		image: 'https://i.imgur.com/DvmN8Hx.jpg',
-		user: 'Katerina',
-		userProfile: 'https://s7.postimg.cc/hsk2th5tn/6_3x.png',
-	},
+		image: study4,
+		
+		
+	},	
 ];
